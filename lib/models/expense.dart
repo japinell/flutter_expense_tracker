5c2aa3f060044ea334b86bb048fdd92d082d3f96
Jose A. Pinell <japinell@yahoo.com>
2025-07-19 16:52:50 -0700

    Added category to the Expense data model

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,14 +2,21 @@ import "package:uuid/uuid.dart";
 
 const uuid = Uuid();
 
+enum Category { food, travel, leisure, work }
+
 class Expense {
   // Title, amount, and date are required parameters for the Expense class.
   // The id is generated using the uuid package.
-  Expense({required this.title, required this.amount, required this.date})
-    : id = uuid.v4();
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4();
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 }
