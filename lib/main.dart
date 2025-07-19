3898113c5e7f1cd086fb328aef154f0fdccea474
Jose A. Pinell <japinell@yahoo.com>
2025-07-19 16:41:08 -0700

    Added boilerplate code for the Expenses screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
+import "package:flutter_expense_tracker/expenses.dart";
 
 void main() => runApp(const MyApp());
 
@@ -7,6 +8,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MyApp();
+    return MaterialApp(home: Expenses());
   }
 }
