541cd944c31e2aaef596ef5b4e2afce2cde1953e
Jose A. Pinell <japinell@yahoo.com>
2025-07-20 14:34:15 -0700

    Enabled Theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import "package:flutter/material.dart";
 import "package:flutter_expense_tracker/widgets/expenses.dart";
 
+final kColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 96, 59, 181),
+  //seedColor: Color.fromARGB(255, 4, 170, 54),
+);
+
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -8,6 +13,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Expenses());
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
+      home: Expenses(),
+    );
   }
 }
